a71570e1fefcb0824aae69bdffafbbc3f0c14587
gregyedlik <greg@yedlik.no>
2021-10-31 20:49:41 +0100

    Checks the range of each byte in each message... And finds an even distribution.

--- repetitions.py
+++ repetitions.py
@@ -1,7 +1,9 @@
 import pandas
+import matplotlib.pyplot as plt
 
 df = pandas.read_csv('list4.csv', header=None)
 df.columns = ['ID', 'data', 'timestamp']
+df['Int'] = [int(proba.replace(' ', ''), 16) for proba in df.data]
 
 arbIDs = set(df.ID)
 dataframes = {arbID: df[df.ID == arbID] for arbID in arbIDs}
@@ -18,18 +20,58 @@ dataframes = {arbID: df[df.ID == arbID] for arbID in arbIDs}
 #     print(f'{ratio*100:.2f}% there!')
 
 # Evaluating how quickly the hit ratio increases:
-df72 = dataframes['0x72']
-points = range(5000, 40000, 10000)
-for limit in points:
-    dataframe = df72[:limit]
-    histogram = dataframe.data.value_counts()
-    repeating_at_least_once = len(histogram[histogram > 1])
-    ratio = repeating_at_least_once / len(histogram)
-    print(f'{ratio*100:.2f}% @ ' + str(limit))
+# df72 = dataframes['0x72'].copy()
+# points = range(5000, len(df72.index), 5000)
+# for limit in points:
+#     dataframe = df72[:limit]
+#     histogram = dataframe.data.value_counts()
+#     repeating_at_least_once = len(histogram[histogram > 1])
+#     ratio = repeating_at_least_once / len(histogram)
+#     print(f'{ratio*100:.2f}% @ ' + str(limit))
+
+
+# Seeing if there is any gap in the numbers covering the range they have available:
+# fig, ax = plt.subplots(2, 2)
+# ax[0, 0].hist(dataframes['0x72'].Int, bins=1000)
+# ax[0, 0].set_xlabel('Data as 64bit unsigned int, 0x72')
+# ax[0, 1].hist(dataframes['0x73'].Int, bins=1000)
+# ax[0, 1].set_xlabel('Data as 64bit unsigned int, 0x73')
+# ax[1, 0].hist(dataframes['0x80'].Int, bins=1000)
+# ax[1, 0].set_xlabel('Data as 64bit unsigned int, 0x80')
+# ax[1, 1].hist(dataframes['0x81'].Int, bins=1000)
+# ax[1, 1].set_xlabel('Data as 64bit unsigned int, 0x81')
+# plt.show()
+
+# This has only really proved that the first byte has full range. Let's bitbang a bit!
+interesting = '0x81'
+fig, ax = plt.subplots(4, 2)
+filtered = [(i & int('0xFF00000000000000', 16)) >> 56 for i in dataframes[interesting].Int]
+ax[0, 0].hist(filtered, bins=256)
+ax[0, 0].set_xlabel(interesting + ', MSB')
+filtered = [(i & int('0x00FF000000000000', 16)) >> 48 for i in dataframes[interesting].Int]
+ax[0, 1].hist(filtered, bins=256)
+filtered = [(i & int('0x0000FF0000000000', 16)) >> 40 for i in dataframes[interesting].Int]
+ax[1, 0].hist(filtered, bins=256)
+filtered = [(i & int('0x000000FF00000000', 16)) >> 32 for i in dataframes[interesting].Int]
+ax[1, 1].hist(filtered, bins=256)
+filtered = [(i & int('0x00000000FF000000', 16)) >> 24 for i in dataframes[interesting].Int]
+ax[2, 0].hist(filtered, bins=256)
+filtered = [(i & int('0x0000000000FF0000', 16)) >> 16 for i in dataframes[interesting].Int]
+ax[2, 1].hist(filtered, bins=256)
+filtered = [(i & int('0x000000000000FF00', 16)) >> 8 for i in dataframes[interesting].Int]
+ax[3, 0].hist(filtered, bins=256)
+filtered = [i & int('0x00000000000000FF', 16) for i in dataframes[interesting].Int]
+ax[3, 1].hist(filtered, bins=256)
+ax[3, 1].set_xlabel(interesting + ', LSB')
+plt.show()
+
+# They look all the same, and all flat.. all bits take all values.
+
+
+
 
 # Todo:
 # Can we estimate the time needed to capture everything?
 # How could we systematically notice patterns in the data?
-    # Some bytes have limited range?
 # Could I use machine learning to pick it up?!
 # Could I collect data faster by bombarding a battery with 72/73?
